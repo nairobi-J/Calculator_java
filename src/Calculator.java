b865e74f67e8b9ab239c513899659f5785ad6802
nairobi-J <jerinusrat001@gmail.com>
2023-06-07 04:07:32 +0600

    day3

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,28 +1,33 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+
+import static java.lang.Math.sqrt;
+
 public class Calculator implements ActionListener{
     JFrame frame;
     JTextField textfield;
     JButton[] numberButtons = new JButton[10];
-    JButton[] functionButtons = new JButton[8];
+    JButton[] functionButtons = new JButton[14];
     JButton addButton, subButton, divButton, mulButton;
-    JButton decButton, equButton, delButton, clrButton;
+    JButton decButton, equButton, delButton, clrButton, negButton,perButton,  sqrtButton, squareButton, lala_land;
     JPanel panel; //space
-    Font myFont = new Font("Ink Free", Font.BOLD, 30);
+    Font myFont = new Font("Ink free", Font.BOLD, 30);
     double num1 = 0, num2 = 0, result = 0;
     String operator;
     Calculator()
     {
         frame = new JFrame("CALCULATOR");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(420, 550);
+        frame.setSize(550, 750);
         frame.setLayout(null);
+        frame.setBackground(Color.black);
 
         textfield = new JTextField();
-        textfield.setBounds(50, 25, 300, 50);
+        textfield.setBounds(55, 25, 400, 50);
         textfield.setFont(myFont);
         textfield.setEditable(false); //cant write anymore
+        textfield.setBackground(Color.white);
 
         addButton = new JButton("+");
         subButton = new JButton("-");
@@ -32,6 +37,20 @@ public class Calculator implements ActionListener{
         equButton = new JButton("=");
         delButton = new JButton("<-");
         clrButton = new JButton("clear");
+        negButton = new JButton("(-)");
+        sqrtButton = new JButton("root");
+        perButton = new JButton("%");
+        squareButton = new JButton("SQR");
+        lala_land = new JButton("~");
+
+
+        addButton.setBackground(Color.orange);
+        subButton.setBackground(Color.magenta);
+        divButton.setBackground(Color.red);
+        mulButton.setBackground(Color.green);
+        negButton.setBackground(Color.gray);
+        delButton.setBackground(Color.gray);
+        clrButton.setBackground(Color.gray);
 
         functionButtons[0] = addButton;
         functionButtons[1] = subButton;
@@ -41,9 +60,21 @@ public class Calculator implements ActionListener{
         functionButtons[5] = equButton;
         functionButtons[6] = delButton;
         functionButtons[7] = clrButton;
+        functionButtons[8] = negButton;
+        functionButtons[9] = perButton;
+        functionButtons[10] = sqrtButton;
+        functionButtons[11] = squareButton;
+        functionButtons[12] = lala_land;
+        perButton.setBackground(Color.yellow);
+        sqrtButton.setBackground(Color.yellow);
+        squareButton.setBackground(Color.yellow);
+        lala_land.setBackground(Color.yellow);
+        equButton.setBackground(Color.CYAN);
+        decButton.setBackground(Color.CYAN);
+
 
        // functionButtons[0] = addButton;
-        for(int i = 0; i < 8; i++)
+        for(int i = 0; i < 13; i++)
         {
             functionButtons[i].addActionListener(this);
             functionButtons[i].setFont(myFont);
@@ -56,16 +87,18 @@ public class Calculator implements ActionListener{
             numberButtons[i].addActionListener(this);
             numberButtons[i].setFont(myFont);
             numberButtons[i].setFocusable(false);
+            numberButtons[i].setBackground(Color.pink);
 
         }
-        delButton.setBounds(50,430,145,50);
-        clrButton.setBounds(205,430,145,50);
+        negButton.setBounds(55,600,120,50);
+        delButton.setBounds(190,600,120,50);
+        clrButton.setBounds(330,600,120,50);
 
         panel = new JPanel();
 
-        panel.setBounds(50, 100, 300, 300);
-        panel.setLayout(new GridLayout(4, 4, 10, 10));
-        panel.setBackground(Color.BLACK);
+        panel.setBounds(55, 80, 400, 450);
+        panel.setLayout(new GridLayout(5, 5, 5, 5));
+        panel.setBackground(Color.black);
         panel.add(numberButtons[1]);
         panel.add(numberButtons[2]);
         panel.add(numberButtons[3]);
@@ -82,11 +115,18 @@ public class Calculator implements ActionListener{
         panel.add(numberButtons[0]);
         panel.add(equButton);
         panel.add(mulButton);
+        panel.add(perButton);
+        panel.add(sqrtButton);
+        panel.add(squareButton);
+        panel.add(lala_land);
+
 
         frame.add(panel);
+        frame.add(negButton);
         frame.add(delButton);
         frame.add(clrButton);
         frame.add(textfield);
+        frame.setBackground(Color.black);
         frame.setVisible(true);
     }
 
@@ -125,6 +165,24 @@ public class Calculator implements ActionListener{
             operator = "x";
             textfield.setText("");
         }
+        if(e.getSource() == perButton)
+        {
+            num1 = Double.parseDouble(textfield.getText());
+            operator = "%";
+            textfield.setText("");
+        }
+        if(e.getSource() == sqrtButton)
+        {
+            num1 = Double.parseDouble(textfield.getText());
+            operator = "root";
+            textfield.setText("");
+        }
+        if(e.getSource() == squareButton)
+        {
+            num1 = Double.parseDouble(textfield.getText());
+            operator = "SQR";
+            textfield.setText("");
+        }
 
         if(e.getSource() == divButton) {
             num1 = Double.parseDouble(textfield.getText());
@@ -150,9 +208,54 @@ public class Calculator implements ActionListener{
                 case"x":
                     result = num1*num2;
                     break;
+
+
             }
             textfield.setText(String.valueOf(result));
             num1 = result;
+        }
+        if(e.getSource() == lala_land) {
+
+            switch(operator)
+            {
+                case"%":
+                {
+                    result = num1/100;
+                    textfield.setText(String.valueOf(result) + "%");
+                    break;
+                }
+                case"root":
+                {
+
+                    result = sqrt(num1);
+                    textfield.setText(String.valueOf(result));
+                    break;
+                }
+                case"SQR":
+                    result = num1*num1;
+                    textfield.setText(String.valueOf(result));
+                    break;
+
+            }
+
+            num1 = result;
+
+        }
+
+        if(e.getSource() == clrButton) {
+
+            textfield.setText("");
+        }
+        if(e.getSource() == delButton) {
+            String string = textfield.getText();
+            textfield.setText("");
+            for(int i = 0; i <  string.length() - 1; i++)
+                textfield.setText(textfield.getText() + string.charAt(i));
+        }
+        if(e.getSource() == negButton) {
+           double temp = Double.parseDouble(textfield.getText());
+           temp *= -1;
+           textfield.setText(String.valueOf(temp));
         }
 
         }
